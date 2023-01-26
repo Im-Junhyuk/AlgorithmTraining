1fa251fe02cdc3586b2da745e784068ed81489d5
Im-Junhyuk <123653481+Im-Junhyuk@users.noreply.github.com>
2023-01-27 01:06:48 +0900

    [Silver III] Title: N과 M (3), Time: 1224 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.py
@@ -0,0 +1,25 @@
+import sys
+
+def dfs():
+    global N,M
+    if len(result) == M:
+        print(' '.join([str(x) for x in result]))
+        return
+    for i in range(1, N+1):
+        '''if result:
+            if i < max(result):
+                continue
+        if visit[i]:
+            continue'''
+
+        #visit[i] = True
+        result.append(i)
+        dfs()
+        #visit[i] = False
+        result.pop()
+
+
+N, M = map(int, sys.stdin.readline().strip().split())
+result = []
+#visit = [False] * (N + 1)
+dfs()
