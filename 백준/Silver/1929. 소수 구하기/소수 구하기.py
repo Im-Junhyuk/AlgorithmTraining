293c760247f37b16d4e2d95d0f0d897b5abc88f5
Im-Junhyuk <123653481+Im-Junhyuk@users.noreply.github.com>
2023-01-27 01:03:04 +0900

    [Silver III] Title: 소수 구하기, Time: 3180 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -0,0 +1,15 @@
+def IsPrime(num):
+    if num == 1:
+        return False
+    elif num == 2:
+        return True
+    else:
+        for i in range(2,int(num**0.5)+1):
+            if num%i ==0:
+                return False
+        return True
+
+M,N = map(int,input().split())
+for i in range(M,N+1):
+    if IsPrime(i):
+        print(i)
