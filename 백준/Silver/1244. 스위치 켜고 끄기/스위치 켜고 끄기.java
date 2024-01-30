fffb8c189ecd3910e40df5c0484f85794cbb1ea9
Im-Junhyuk <123653481+Im-Junhyuk@users.noreply.github.com>
2024-01-30 17:15:40 +0900

    [Silver IV] Title: 스위치 켜고 끄기, Time: 148 ms, Memory: 14640 KB -BaekjoonHub

--- 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.java
+++ 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.java
@@ -1,54 +1,84 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+
+;
 
 public class Main {
-
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		int[] state = new int[n];
-		for (int i=0; i<n; i++) {
-			state[i] = sc.nextInt();
+	static int[] switchArr;
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int switchNum = Integer.parseInt(br.readLine());
+		switchArr = new int[switchNum];
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < switchNum; i++) {
+			switchArr[i] = Integer.parseInt(st.nextToken());
 		}
 		
-		int sn = sc.nextInt();
-		for (int i=0; i<sn; i++) {
-			int a = sc.nextInt();
-			int b = sc.nextInt();
+		int personNum = Integer.parseInt(br.readLine());
+		for(int i = 0; i < personNum; i++) {
+			st = new StringTokenizer(br.readLine());
+			int gender = Integer.parseInt(st.nextToken());
+			int sNum = Integer.parseInt(st.nextToken());
 			
-			if (a == 1) {
-				for (int ms = b-1; ms < n; ms += b) { //스위치 번호의 배수, 단 스위치의 인덱스는 스위치 번호의 -1
-					state[ms] = 1 - state[ms];
-				}
+			if(gender == 1) {
+				turnOnAndOffMan(sNum);
 			}
-			else if (a == 2) {
-				int ws = b-1;
-				state[ws] = 1 - state[ws];
-				int left = ws-1;
-				int right = ws+1;
-				while (true) {
-					if (left >= 0 && right < n && state[left] == state[right]) {
-						state[left] = 1 - state[left];
-						state[right] = 1 - state[right];
-						left--;
-						right++;
-					}
-					else {
-						break;
-					}
-				}
+			if(gender == 2) {
+				turnOnAndOffWoman(sNum);
 			}
-			
 		}
-		int br = (n-1)/20;
 		
-		for (int i=0; i<=br; i++) {
-			for (int j=i*20; j<(i+1)*20; j++) {
-				if(j>=n) {
-					break;
+		StringBuilder sb = new StringBuilder();
+		for(int i = 0; i < switchArr.length; i++) {
+			if(i % 20 == 0&& i != 0)
+				bw.write("\n");
+			bw.write(switchArr[i] + " ");
+		}
+		bw.flush();
+	}
+	public static void turnOnAndOffMan(int sNum) {
+		for(int i = 0; i < switchArr.length; i++) {
+			if((i+1) % sNum == 0) {
+				if(switchArr[i] == 0)
+					switchArr[i] = 1;
+				else {
+					switchArr[i] = 0;
 				}
-				System.out.printf("%d ", state[j]);
 			}
-			System.out.println();
+		}
+	}
+	public static void turnOnAndOffWoman(int sNum) {
+		sNum--;
+		if(switchArr[sNum] == 0)
+			switchArr[sNum] = 1;
+		else 
+			switchArr[sNum] = 0;
+		
+		for(int i = 1; i < switchArr.length; i++) {
+			int left = sNum - i;
+			int right = sNum + i;
+			if(left < 0 || right >= switchArr.length)
+				break;
+			
+			if(switchArr[left] != switchArr[right])
+				break;
+			
+			if(switchArr[left] == 0) {
+				switchArr[left] = 1;
+				switchArr[right] = 1;
+			} else {
+				switchArr[left] = 0;
+				switchArr[right] = 0;
+			}
 		}
 	}
 
