5570ed2e4a7570a1fac28b515f7514b880357e50
Im-Junhyuk <123653481+Im-Junhyuk@users.noreply.github.com>
2024-01-31 10:30:42 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 552 ms, Memory: 53076 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -1,44 +1,43 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
-		int N, M;
+		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		
-		int[] intArray = new int[N];
+		StringTokenizer st;
+		st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
 		
 		st = new StringTokenizer(br.readLine());
+		int[] array = new int[n];
+		int[] accumulate = new int[n+1];
+		int acc = 0;
 		
-		for(int i = 0; i < N; i++) {
-			intArray[i] = Integer.parseInt(st.nextToken());
-		}
-		
-		int[] sumArray = new int[N+1];
-		for(int i = 1; i < N+1; i++) {
-			sumArray[i] = sumArray[i-1] + intArray[i-1];
+		for (int i=0; i<n; i++) {
+			array[i] = Integer.parseInt(st.nextToken());
+			acc += array[i];
+			accumulate[i+1] = acc;
 		}
 
-
-		for(int i = 0; i < M; i++) {
+		
+		StringBuilder sb = new StringBuilder();
+		
+		
+		for (int t=0; t<m; t++) {
 			st = new StringTokenizer(br.readLine());
-			int fir = Integer.parseInt(st.nextToken());
-			int sec = Integer.parseInt(st.nextToken());
-			int sum = sumArray[sec] - sumArray[fir-1];
-			bw.write(sum + "\n");
+			int i= Integer.parseInt(st.nextToken());
+			int j= Integer.parseInt(st.nextToken());
+			
+					
+			sb.append(accumulate[j]-accumulate[i-1]).append("\n");
 		}
+		System.out.println(sb);
 		
-		bw.flush();
 	}
 
 }
