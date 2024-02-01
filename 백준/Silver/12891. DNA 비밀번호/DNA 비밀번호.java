7fec34a493903c874e415fe33f72bd5543eba43a
Im-Junhyuk <123653481+Im-Junhyuk@users.noreply.github.com>
2024-02-01 11:01:39 +0900

    [Silver II] Title: DNA 비밀번호, Time: 296 ms, Memory: 85736 KB -BaekjoonHub

--- 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.java
+++ 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.java
@@ -1,75 +1,67 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
-
-		// get input
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		int sLength = Integer.parseInt(st.nextToken());
-		int pLength = Integer.parseInt(st.nextToken());
-		
-		String s = br.readLine();
-		
+		StringTokenizer st = null;
 		st = new StringTokenizer(br.readLine());
-		int A = Integer.parseInt(st.nextToken());
-		int C = Integer.parseInt(st.nextToken());
-		int G = Integer.parseInt(st.nextToken());
-		int T = Integer.parseInt(st.nextToken());
-		
-		int letter[] = new int[4];
-		
-		// init
-		for(int i = 0; i < pLength; i++) {
-			letter[charToInt(s.charAt(i))]++;
+		int s = Integer.parseInt(st.nextToken());
+		int p = Integer.parseInt(st.nextToken());
+		String dna = br.readLine();
+		int[] countACGT = new int[4];
+		st = new StringTokenizer(br.readLine());
+		for (int i=0; i<4; i++) {
+			countACGT[i] = Integer.parseInt(st.nextToken());
 		}
 		
+		int[][] accCountACGT = new int[s+1][4];
+		for(int i=0; i<4; i++) {
+			accCountACGT[0][i] = 0;
+		}
 		
-		// slicing string
-		int start = 0;
-		int end = sLength - pLength + 1;
-		int count = 0;
-		
-		while(true) {
-			// check that is good
-			if(letter[0] >= A && letter[1] >= C && letter[2] >= G && letter[3] >= T)
-				count++;
-			
-			// go to next
-			if(start+1 >= end)
-				break;
-			
-			letter[charToInt(s.charAt(start))]--;
-			letter[charToInt(s.charAt(start + pLength))]++;
-			
-			start++;
 
+		for (int i=0; i<s; i++) {
+			accCountACGT[i+1] = accCountACGT[i].clone();
+			if (dna.charAt(i) == 'A') {
+				accCountACGT[i+1][0]++;
+			}
+			else if (dna.charAt(i) == 'C') {
+				accCountACGT[i+1][1]++;
+			}
+			else if (dna.charAt(i) == 'G') {
+				accCountACGT[i+1][2]++;
+			}
+			else if (dna.charAt(i) == 'T') {
+				accCountACGT[i+1][3]++;
+			}
 		}
-		bw.write(String.valueOf(count));
-		bw.flush();
 		
+		int ans = 0;
+		boolean flag = false;
+
+		for (int i=p; i<s+1; i++) { //i번째와 i-p번째를 비교, 0번째 인덱스는 0으로 새로 추가하여 초기화
+			for (int j=0; j<4; j++) { //A, C, G, T 검사
+				if (accCountACGT[i][j] - accCountACGT[i-p][j] < countACGT[j]) { //주어진 개수보다 모자란 경우
+					flag = true; //i문을 다음단계로 넘기기 위해 for문 탈출 flag
+					break;
+				}
+			}
+			if (flag == true) {
+				flag = false;
+				continue;
+			}
+			ans += 1; //주어진 개수를 모두 만족한 경우
+		}
+		
+		System.out.println(ans);
+		
+
 	}
 	
-	static int charToInt(char c) {
-		switch (c) {
-		case 'A':
-			return 0;
-		case 'C':
-			return 1;
-		case 'G':
-			return 2;
-		case 'T':
-			return 3;
-		}
-		return 0;
-	}
 
 }
