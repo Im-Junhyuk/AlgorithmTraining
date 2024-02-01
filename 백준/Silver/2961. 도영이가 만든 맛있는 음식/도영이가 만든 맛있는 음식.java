f1816f6eb734613bb8e8a8cf11a8a3e22fe32458
Im-Junhyuk <123653481+Im-Junhyuk@users.noreply.github.com>
2024-02-01 13:26:12 +0900

    [Silver II] Title: 도영이가 만든 맛있는 음식, Time: 128 ms, Memory: 14176 KB -BaekjoonHub

--- 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.java
+++ 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.java
@@ -1,63 +1,51 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Main {
 
-	static int N;
-	static int min, s, b, result;
-	static int[] sour, bitter;
-	static boolean[] isSelected;
-	static boolean flag; //적어도 하나의 재료는 사용해야 한다.
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		// TODO Auto-generated method stub
+	static int[] sour;
+	static int[] bitter;
+	
+	public static void main(String[] args) throws IOException {
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = null;
-		N = Integer.parseInt(br.readLine());
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+		int N = Integer.parseInt(br.readLine());
 		sour = new int[N];
 		bitter = new int[N];
-		isSelected = new boolean[N];
-		min = Integer.MAX_VALUE;
-		for (int i=0; i<N; i++) {
+
+		
+		for(int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
 			sour[i] = Integer.parseInt(st.nextToken());
 			bitter[i] = Integer.parseInt(st.nextToken());
 		}
-		subset(0);
-		System.out.println(min);
 		
-	}
-	
-	public static void subset(int cnt) {
-		if (cnt == N) {
-			flag = false;
-			s = 1;
-			b = 0;
-			for (int i=0; i<N; i++) {
-				if (isSelected[i]) {
-					flag = true;
-					s = s * sour[i];
-					b = b + bitter[i];
+		int minGap = Integer.MAX_VALUE;
+		int maxV = (int) Math.pow(2, N); 
+		
+		for(int i = 1; i < maxV; i++) {
+			int sum = 0;
+			int mul = 1;
+			int bit = 1;
+			
+			for(int j = 0; j < N; j++) {
+				if((i & (bit << j)) != 0) { // it chosen
+					mul *= sour[j];
+					sum += bitter[j];
 				}
 			}
-			if (flag == false) { //한개도 고르지 않았다면 무효
-				return;
-			}
-			result = Math.abs(s-b);
-			if (result < min) {
-				min = result;
-			}
-			return;
+			
+			minGap = Math.min(minGap, Math.abs(sum - mul));
 		}
 		
-		else {
-			isSelected[cnt] = true; //cnt번째 입력을 부분집합에 포함
-			subset(cnt+1);
-			isSelected[cnt] = false; //cnt번째 입력을 부분집합에 포함X
-			subset(cnt+1);
-		}
+		bw.write(String.valueOf(minGap));
+		bw.flush();
 	}
 
 }
