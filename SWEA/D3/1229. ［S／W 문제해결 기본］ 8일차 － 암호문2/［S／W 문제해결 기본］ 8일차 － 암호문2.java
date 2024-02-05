a1ccaab8bbe13e9526c9d9922443895725f09bfb
Im-Junhyuk <123653481+Im-Junhyuk@users.noreply.github.com>
2024-02-05 16:31:30 +0900

    [D3] Title: [S/W 문제해결 기본] 8일차 - 암호문2, Time: 106 ms, Memory: 19,320 KB -BaekjoonHub

---
+++ SWEA/D3/1229. ［S／W 문제해결 기본］ 8일차 － 암호문2/［S／W 문제해결 기본］ 8일차 － 암호문2.java
@@ -0,0 +1,189 @@
+import java.util.StringTokenizer;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+/*
+   사용하는 클래스명이 Solution 이어야 하므로, 가급적 Solution.java 를 사용할 것을 권장합니다.
+   이러한 상황에서도 동일하게 java Solution 명령으로 프로그램을 수행해볼 수 있습니다.
+ */
+class Solution
+{
+
+	public static void main(String args[]) throws Exception
+	{
+		/*
+		   아래의 메소드 호출은 앞으로 표준 입력(키보드) 대신 input.txt 파일로부터 읽어오겠다는 의미의 코드입니다.
+		   여러분이 작성한 코드를 테스트 할 때, 편의를 위해서 input.txt에 입력을 저장한 후,
+		   이 코드를 프로그램의 처음 부분에 추가하면 이후 입력을 수행할 때 표준 입력 대신 파일로부터 입력을 받아올 수 있습니다.
+		   따라서 테스트를 수행할 때에는 아래 주석을 지우고 이 메소드를 사용하셔도 좋습니다.
+		   단, 채점을 위해 코드를 제출하실 때에는 반드시 이 메소드를 지우거나 주석 처리 하셔야 합니다.
+		 */
+		//System.setIn(new FileInputStream("res/input.txt"));
+
+		/*
+		   표준입력 System.in 으로부터 스캐너를 만들어 데이터를 읽어옵니다.
+		 */
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = 10;
+		/*
+		   여러 개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
+		*/
+
+		for(int test_case = 1; test_case <= T; test_case++)
+		{
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			LinkedList linkedList = new LinkedList();
+			int cryptoNum = Integer.parseInt(st.nextToken());
+			int[] s = new int[cryptoNum];
+			
+			st = new StringTokenizer(br.readLine());
+			for(int i = 0; i < cryptoNum; i++) {
+				s[i] = Integer.parseInt(st.nextToken());
+			}
+			
+			linkedList.add(cryptoNum, s);
+			
+			int instNum = Integer.parseInt(br.readLine());
+			st = new StringTokenizer(br.readLine());
+			for(int i = 0; i < instNum; i++) {
+
+				String inst = st.nextToken();
+				int x, y;
+				int list[];
+				switch (inst) {
+					case "I": 
+						x = Integer.parseInt(st.nextToken());
+						y = Integer.parseInt(st.nextToken());
+						list = new int[y];
+						for(int j = 0; j < y; j++) {
+							list[j] = Integer.parseInt(st.nextToken());
+						}
+						linkedList.insert(x, y, list);
+						break;
+					case "D": 
+						x = Integer.parseInt(st.nextToken());
+						y = Integer.parseInt(st.nextToken());
+						linkedList.delete(x, y);
+						break;
+					case "A":
+						y = Integer.parseInt(st.nextToken());
+						list = new int[y];
+						for(int j = 0; j < y; j++) {
+							list[j] = Integer.parseInt(st.nextToken());
+						}
+						linkedList.add(y, list);	
+				}
+				
+
+			}
+			BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+			bw.write("#" + test_case + " " + linkedList.get10Nodes() + "\n");
+			bw.flush();
+
+		}
+	}
+
+	public static class LinkedList{
+		Node head;
+		Node tail;
+
+		
+		public LinkedList() {
+			head = null;
+			tail = null;
+		}
+		
+		public Node getNode(int info) {
+			return new Node(info);
+		}
+		
+		public void insert(int x, int y, int[] s) {
+
+			int start = 0;
+			
+			if(x == 0) {
+				Node curNode = getNode(s[0]);
+				curNode.next = head;
+				head = curNode;
+				start++;
+				x++;
+			}
+			
+			Node startNode = head;
+			for(int idx = 1; idx < x; idx++) {
+				startNode = startNode.next;
+			}
+			
+			Node linkNode = startNode.next;
+			
+			for(; start < y; start++) {
+				Node curNode = getNode(s[start]);
+				startNode.next = curNode;
+				startNode = curNode;
+			}
+			
+			startNode.next = linkNode;
+			if(startNode.next == null)
+				tail = startNode;
+		}
+		
+		public void delete(int x, int y) {
+			Node startNode = head;
+			for(int idx = 1; idx < x; idx++) {
+				startNode = startNode.next;
+			}
+			if (x == 0)
+				y--;
+			
+			Node linkNode = startNode;
+			for(int i = 0; i < y+1; i++) {
+				linkNode = linkNode.next;
+			}
+			startNode.next = linkNode;
+			
+			if(x == 0)
+				head = linkNode;
+			
+			if (linkNode == null)
+				tail = startNode;
+		}
+		
+		public void add(int y, int[] s) {
+			int index = 0;
+			Node curNode;
+			if(head == null) {
+				curNode = getNode(s[index]);
+				head = curNode;
+				tail = curNode;
+				index++;
+			}
+			for(; index < y; index++) {
+				curNode = getNode(s[index]);
+				tail.next = curNode;
+				tail = curNode;
+			}
+		}
+		
+		public String get10Nodes() {
+			Node curNode = head;
+			StringBuilder sb = new StringBuilder();
+			for(int i = 0; i < 10; i++) {
+				sb.append(curNode.info + " ");
+				curNode = curNode.next;
+			}
+			return sb.toString();
+		}
+	}
+	
+	public static class Node{
+		int info;
+		Node next;
+		
+		public Node(int info) {
+			this.info = info;
+			this.next = null;
+		}
+	}
+}
