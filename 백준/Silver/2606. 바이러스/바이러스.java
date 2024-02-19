8e40e6219d73395775e7d2f900f241eb3bb7b116
Im-Junhyuk <123653481+Im-Junhyuk@users.noreply.github.com>
2024-02-19 17:49:08 +0900

    [Silver III] Title: 바이러스, Time: 76 ms, Memory: 11604 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -1,45 +1,48 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
-class Main{
-    public static void main(String[] args) throws IOException{
-    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
- 
-    	int v = Integer.parseInt(br.readLine());
-    	int e = Integer.parseInt(br.readLine());
-    	int len = v + 1;
-    	int[][] edge = new int[len][len];
-    	
-    	for(int i = 0; i<e; i++) {
-    		StringTokenizer st = new StringTokenizer(br.readLine());
-    		int v1 = Integer.parseInt(st.nextToken());
-    		int v2 = Integer.parseInt(st.nextToken());
-    		edge[v1][v2] = 1;
-    		edge[v2][v1] = 1;
-    	}
-    	Queue<Integer> q = new LinkedList<Integer>();
-    	q.add(1);
-    	int[] visited = new int[len];
-    	int addicted = -1;
-    	
-    	while(q.isEmpty() == false) {
-    		int cur_v = q.poll();
-    		if (visited[cur_v] == 1)
-    			continue;
-    		visited[cur_v] = 1;
-    		addicted++;
-    		
-    		for(int i = 0; i < len; i++) {
-    			if (edge[cur_v][i]== 1 && visited[i]== 0) {
-    				q.add(i);
-    			}
-    		}
-    	}
-    	System.out.println(addicted);
-    }
+public class Main {
+
+	static boolean[][] graph;
+	static boolean[] visited;
+	static int addicted;
+	static int com;
+	
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st ;
+		StringBuilder sb = new StringBuilder();
+		
+		st = new StringTokenizer(br.readLine());
+		com = Integer.parseInt(st.nextToken());
+		int edge = Integer.parseInt(br.readLine());
+		
+		graph = new boolean[com+1][com+1];
+		for(int i = 0; i < edge; i++) {
+			st = new StringTokenizer(br.readLine());
+			int c1 = Integer.parseInt(st.nextToken());
+			int c2 = Integer.parseInt(st.nextToken());
+			
+			graph[c1][c2] = true;
+			graph[c2][c1] = true;
+		}
+		
+		visited = new boolean[com+1];
+		addicted = -1;
+		dfs(1);
+		System.out.println(addicted);
+
+	}
+	static void dfs(int c) {
+		visited[c] = true;
+		addicted++;
+		
+		for(int i = 1; i < com+1; i++) {
+			if(graph[c][i] && !visited[i])
+				dfs(i);
+		}
+	}
 }
