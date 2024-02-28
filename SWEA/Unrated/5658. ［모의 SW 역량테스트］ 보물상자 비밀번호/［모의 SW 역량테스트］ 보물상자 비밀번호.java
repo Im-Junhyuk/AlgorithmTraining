af9673e205a06965785c0761cc4d7454024e9846
Im-Junhyuk <123653481+Im-Junhyuk@users.noreply.github.com>
2024-02-28 15:33:10 +0900

    [Unrated] Title: [모의 SW 역량테스트] 보물상자 비밀번호, Time: 111 ms, Memory: 18,868 KB -BaekjoonHub

--- SWEA/Unrated/5658. ［모의 SW 역량테스트］ 보물상자 비밀번호/［모의 SW 역량테스트］ 보물상자 비밀번호.java
+++ SWEA/Unrated/5658. ［모의 SW 역량테스트］ 보물상자 비밀번호/［모의 SW 역량테스트］ 보물상자 비밀번호.java
@@ -1,9 +1,12 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.StringTokenizer;
 
 public class Solution {
@@ -22,7 +25,7 @@ public class Solution {
 			st = new StringTokenizer(br.readLine());
 			int N = Integer.parseInt(st.nextToken());
 			int K = Integer.parseInt(st.nextToken());
-			Integer[] candidate = new Integer[N];
+			Set<Integer> set = new HashSet<>();
 			
 			String str = br.readLine();
 			for(int i = 0; i < N; i++) {
@@ -31,17 +34,19 @@ public class Solution {
 					temp[j-i] = str.charAt(j%N);
 				}
 				
-				candidate[i] = Integer.parseInt(String.valueOf(temp), 16);
+				set.add(Integer.parseInt(String.valueOf(temp), 16));
 			}
 			
-			Arrays.sort(candidate, Collections.reverseOrder());
-			//for(int i = 0; i < N; i++) {
-			//	System.out.print(candidate[i] + " ");
-			//}System.out.println();
+			ArrayList<Integer> candidate = new ArrayList<>();
+			candidate.addAll(set);
+			Collections.sort(candidate, Collections.reverseOrder());
+//			for(int i = 0; i < N; i++) {
+//				System.out.print(candidate[i] + " ");
+//			}System.out.println();
 			sb.append("#")
 				.append(t)
 				.append(" ")
-				.append(candidate[K-1])
+				.append(candidate.get(K-1))
 				.append("\n");
 			
 		}
