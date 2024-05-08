4be22e5bf0707762a08a77fa558721d53d92f53c
Im-Junhyuk <123653481+Im-Junhyuk@users.noreply.github.com>
2024-05-08 20:16:28 +0900

    [Bronze V] Title: A/B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+using namespace std;
+
+int main() {
+	long double i, j;
+	cin >> i >> j;
+
+	cout.precision(10);
+	i = i / j;
+	cout << i << endl;
+
+}
