0a5b8c843f08e29730c64c9b6073c36c754085b3
Im-Junhyuk <123653481+Im-Junhyuk@users.noreply.github.com>
2024-08-31 15:31:41 +0900

    [Gold III] Title: 내리막 길, Time: 268 ms, Memory: 36580 KB -BaekjoonHub

--- 백준/Gold/1520. 내리막 길/내리막 길.java
+++ 백준/Gold/1520. 내리막 길/내리막 길.java
@@ -14,10 +14,7 @@ public class Main {
 	static int[] dx = {0, 0, 1, -1};
 	static int[] dy = {1, -1, 0, 0};
 	
-	static int cnt = 0;
-
 	public static void main(String[] args) throws IOException {
-		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
@@ -27,49 +24,48 @@ public class Main {
 		map = new int[N][M];
 		dp = new int[N][M];
 		
-		
-		for(int n = 0; n < N; n++) {
+		for(int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
-			for(int m = 0; m < M; m++) {
-				map[n][m] = Integer.parseInt(st.nextToken());
-				dp[n][m] = -1;
+			for(int j = 0; j < M; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+				dp[i][j] = -1;
 			}
 		}
 		
 		System.out.println(dfs(0, 0));
+//		print();
 	}
 	
 	static int dfs(int x, int y) {
-		if(x == N-1 && y == M-1) {
-			return 1;
-		}
-		
 		if(dp[x][y] != -1)
 			return dp[x][y];
+		
 		dp[x][y] = 0;
+		
 		for(int d = 0; d < 4; d++) {
 			int nx = x + dx[d];
 			int ny = y + dy[d];
 			
-			if(!isInRange(nx, ny))
+			if(nx < 0 || nx >= N || ny < 0 || ny >= M || map[nx][ny] >= map[x][y])
 				continue;
 			
-			if(map[nx][ny] < map[x][y]) {
-				if(dp[nx][ny] == -1)
-					dp[x][y] += dfs(nx, ny);
-				else
-					dp[x][y] += dp[nx][ny];
+			if(nx == N-1 && ny == M-1) {
+				dp[x][y]++;
+				continue;
 			}
+				
+			dp[x][y] += dfs(nx, ny);
 		}
 		
 		return dp[x][y];
-			
 	}
-	
-	static boolean isInRange(int x, int y) {
-		if(x >= 0 && x < N && y >= 0 && y < M)
-			return true;
-		return false;
+
+	static void print() {
+		for(int i = 0; i < N; i++) {
+			for(int j = 0; j < M; j++) {
+				System.out.print(dp[i][j] + " ");
+			}
+			System.out.println();
+		}
 	}
 }
-
