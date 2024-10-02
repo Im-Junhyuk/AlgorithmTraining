9509775fb80570f7ee2db52bf7bc377e3f0cf299
Im-Junhyuk <123653481+Im-Junhyuk@users.noreply.github.com>
2024-10-02 11:25:28 +0900

    [level 2] Title: 소수 찾기, Time: 73.42 ms, Memory: 101 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -8,32 +8,21 @@ class Solution {
     boolean[] primeList;
     int answer;
     boolean[] visited;
-    int maxNum;
-    StringBuilder sb;
-    String nums;
-    int digit;
     
     public int solution(String numbers) {
         
-        nums = numbers;
         len = numbers.length();
         System.out.println(len);
         answer = 0;
         done = new HashSet<>();
-        digit = numbers.length();
-        maxNum = (int)Math.pow(10, digit);
-        sb = new StringBuilder();
-        visited = new boolean[digit];
+        int digit = numbers.length();
         
-        primeList = new boolean[maxNum];
-        
-        // System.out.println("prime start");
+        System.out.print(digit + " , " + Math.pow(10, digit));
+        primeList = new boolean[(int)Math.pow(10, digit)];
         // prime();
-        // System.out.println("prime");
-        permutation(0);
+        // permutation();
         
         
-        System.out.println(answer);
         return answer;
     }
     
@@ -41,59 +30,22 @@ class Solution {
         primeList[0] = true;
         primeList[1] = true;
         
-        for(int i = 2; i <= Math.sqrt(maxNum-1); i++){
+        for(int i = 2; i <= Math.sqrt(9999999); i++){
             int cur = i;
-            if(primeList[cur])
-                continue;
-            
-            cur += i;
-            while(cur < primeList.length){
-                // if(cur > maxNum-1)
-                //     break;
-                primeList[cur] = true;
+            while(true){
                 cur =+ i;
+                if(cur > 9999999)
+                    break;
+                primeList[cur] = true;
+                
             }
         }
     }
     
-    public void permutation(int depth){
-        if(depth != 0){
-            String str = sb.toString();
-            System.out.println(str);
-            int curNum = Integer.valueOf(str);
-            if(done.contains(curNum) == false){
-                done.add(curNum);
-                if(isPrime1(curNum))
-                    answer++;
-            }
-        }
+    public void permutation(){
         
-        if(depth == digit)
-            return;
-        
-        for(int i = 0; i < digit; i++){
-            if(visited[i] == false){
-                visited[i] = true;
-                sb.append(nums.charAt(i));
-                
-                permutation(depth+1);
-                
-                visited[i] = false;
-                sb.deleteCharAt(sb.length()-1);
-            }
-        }
     }
-    public boolean isPrime1(int num){
-        if(num <= 1){
-            return false;
-        }
-        
-        for(int i = 2; i <= Math.sqrt(num); i++){
-            if(num%i == 0)
-                return false;
-        }
-        return true;
-    }
+    
     public boolean isPrime(int num){
         if(primeList[num])
             return false;
